130d738d3e8eda709271935fc54b693f57224dda
rishabhkunwar04 <rishabh.kunwar04@gmail.com>
2023-05-20 00:03:07 +0530

    server added

--- timeZone-assignment/app/server.go
+++ timeZone-assignment/app/server.go
@@ -1,17 +1,53 @@
 package app
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
 func Start() {
 	mux := mux.NewRouter()
-	mux.HandleFunc("/api/time", CurrentTime)
-	mux.HandleFunc("/api/time/{para_m}", regionalTime)
+	mux.HandleFunc("/api/time", getTime)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+
+}
+
+func getTime(w http.ResponseWriter, r *http.Request) {
+
+	response := make(map[string]string, 0)
+	tz := r.URL.Query().Get("tz")
+	timezones := strings.Split(tz, ",")
+
+	if len(timezones) <= 1 {
+		loc, err := time.LoadLocation(tz)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(fmt.Sprintf("invalid timezone %s", loc)))
+		} else {
+			response[loc.String()] = time.Now().In(loc).String()
+			w.Header().Add("Content-type", "application/json")
+			json.NewEncoder(w).Encode(response)
+		}
+	} else {
+		for _, val := range timezones {
+			loc, err := time.LoadLocation(val)
+			if err != nil {
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte(fmt.Sprintf("invalid timezones %s", val)))
+			} else {
+				response[val] = time.Now().In(loc).String()
+				w.Header().Add("Content-type", "application/json")
+				json.NewEncoder(w).Encode(response)
+			}
+		}
+
+	}
 
 }
--- timeZone-assignment/go.mod
+++ timeZone-assignment/go.mod
@@ -1,3 +1,5 @@
-module timeZone-assign
+module banking-system
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
